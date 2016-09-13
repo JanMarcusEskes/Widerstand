ce7f3ea71b8e29c56d38ef840507ae5d3bfd1fb3
Jan-Marcus Eskes <jan-marcus@eskes.de>
2016-09-13 16:47:03 +0200

    Abändern des Verhaltens auf Fehleingaben

--- Wiederstand/Program.cs
+++ Wiederstand/Program.cs
@@ -14,13 +14,13 @@ namespace Wiederstand
             string eingabe;
             Console.WriteLine("Wiederstand 1: ");
             if (Double.TryParse(eingabe = Console.ReadLine(), out R1) == true) R1 = Double.Parse(eingabe);
-            else Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); 
+            else { Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); Console.ReadLine(); return; }
             Console.WriteLine("Wiederstand 2: ");
-            if (Double.TryParse(eingabe = Console.ReadLine(), out R2) == true) R2 = Double.Parse(eingabe); 
-            else Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); 
+            if (Double.TryParse(eingabe = Console.ReadLine(), out R2) == true) R2 = Double.Parse(eingabe);
+            else { Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); Console.ReadLine(); return; }
             Console.WriteLine("Wiederstand 3: ");
             if (Double.TryParse(eingabe = Console.ReadLine(), out R3) == true) R3 = Double.Parse(eingabe);
-            else Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); 
+            else { Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); Console.ReadLine(); return; }
             double Ergebnis = 1 / ((1 / (R1 + R2)) + (1 / R3));
             Console.WriteLine("Der Gesamtwiederstand beträgt: " + Ergebnis + "Ohm");
             Console.ReadLine();
