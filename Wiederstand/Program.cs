9894f5fddabd43d3edc70e9dd6818bc2ecb00671
Admin <Admin@R131PC00>
2016-09-15 16:59:19 +0200

    Entfernen der IF vorraussetzung

--- Wiederstand/Program.cs
+++ Wiederstand/Program.cs
@@ -6,24 +6,21 @@ using System.Threading.Tasks;
 
 namespace Wiederstand
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            double R1, R2, R3;
-            string eingabe;
-            Console.WriteLine("Wiederstand 1: ");
-            if (Double.TryParse(eingabe = Console.ReadLine(), out R1) == true) R1 = Double.Parse(eingabe);
-            else { Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); Console.ReadLine(); return; }
-            Console.WriteLine("Wiederstand 2: ");
-            if (Double.TryParse(eingabe = Console.ReadLine(), out R2) == true) R2 = Double.Parse(eingabe);
-            else { Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); Console.ReadLine(); return; }
-            Console.WriteLine("Wiederstand 3: ");
-            if (Double.TryParse(eingabe = Console.ReadLine(), out R3) == true) R3 = Double.Parse(eingabe);
-            else { Console.WriteLine("Es wurde keine gültige Zahl eingegeben!"); Console.ReadLine(); return; }
-            double Ergebnis = 1 / ((1 / (R1 + R2)) + (1 / R3));
-            Console.WriteLine("Der Gesamtwiederstand beträgt: " + Ergebnis + "Ohm");
-            Console.ReadLine();
-        }
+      double R1, R2, R3;
+      string eingabe;
+      Console.WriteLine("Wiederstand 1: ");
+      R1 = Double.Parse(Console.ReadLine());
+      Console.WriteLine("Wiederstand 2: ");
+      R2 = Double.Parse(Console.ReadLine());
+      Console.WriteLine("Wiederstand 3: ");
+      R3 = Double.Parse(Console.ReadLine());
+      double Ergebnis = 1 / ((1 / (R1 + R2)) + (1 / R3));
+      Console.WriteLine("Der Gesamtwiederstand beträgt: " + Ergebnis + " Ohm");
+      Console.ReadLine();
     }
+  }
 }
